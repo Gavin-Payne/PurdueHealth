b924210e95a2ca57da577c1bbd46e5380c0d667d
leharris2005 <leharris2120@gmail.com>
2025-02-23 00:26:11 -0500

    Revert "first edit after getting website actually working wow"
    This reverts commit b8eb8f9550a483f32a175a0fef189583626a5195.
    
    reverted

--- Backend/server.js
+++ Backend/server.js
@@ -13,11 +13,6 @@ const allowedOrigins = [
   'https://boilerfit.me'
 ];
 
-const allowedOrigins = [
-  'https://localhost:3000',
-  'https://boilerfit.me'
-];
-
 app.use(cors({
   origin: function(origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -26,9 +21,9 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true,  // Make sure credentials (cookies) are allowed
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-User-Email']  // Allow Authorization headers
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-User-Email']
 }));
 
 app.use(express.json());
